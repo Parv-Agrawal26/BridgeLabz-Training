e9a76379b29e5868f6f636caa41cce61f2576a03
Parv Agrawal <agrawalparv262004@gmail.com>
2025-12-18 11:01:29 +0530

    [Parv Agrawal] Find Average of 3 numbers

---
+++ core-java-practice/gcr-codebase/programming-elements/Average.java
@@ -0,0 +1,9 @@
+public class Average {
+    public static void main(String[] args) {
+        int num1 = 15;
+        int num2 = 25;
+        int num3 = 35;
+        double average = (num1 + num2 + num3) / 3.0;
+        System.out.println("The average of " + num1 + ", " + num2 + ", and " + num3 + " is: " + average);
+    }
+}
