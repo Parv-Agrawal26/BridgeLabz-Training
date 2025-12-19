d83b3c7b21454a513e6aae21c743119b8fed3f57
Parv Agrawal <agrawalparv262004@gmail.com>
2025-12-19 21:27:26 +0530

    [Parv Agrawal] Refactor.Refactored class names

--- core-java-practice/gcr-codebase/control-flow/PowerCalculationusingWhile.java
+++ core-java-practice/gcr-codebase/control-flow/PowerCalculationusingWhile.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class PowerCalculation {
+class PowerCalculationUsingWhile {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int number = scn.nextInt();
