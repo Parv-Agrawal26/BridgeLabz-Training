071ae4951178d1ed2a57eaa0b94804248195c5b0
Parv Agrawal <agrawalparv262004@gmail.com>
2025-12-31 18:48:27 +0530

    [Parv Agrawal] Add.added constructor and instance programs

---
+++ oops-practice/gcr-codebase/constructor-and-instance/BankAccount.java
@@ -0,0 +1,19 @@
+public class BankAccount {
+    public int accountNumber;
+    protected String accountHolder;
+    private double balance;
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
+class SavingsAccount extends BankAccount {
+    void display() {
+        System.out.println(accountNumber + " " + accountHolder);
+    }
+}
