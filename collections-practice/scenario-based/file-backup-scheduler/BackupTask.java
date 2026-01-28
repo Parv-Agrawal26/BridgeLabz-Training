aae15e41e28784618378ae2afb49be929561136f
Parv Agrawal <agrawalparv262004@gmail.com>
2026-01-28 16:20:27 +0530

    [Parv Agrawal] Add.solved collections related scenario based question

---
+++ collections-practice/scenario-based/file-backup-scheduler/BackupTask.java
@@ -0,0 +1,23 @@
+public class BackupTask implements Comparable<BackupTask> {
+
+    private String folderPath;
+    private int priority;
+
+    public BackupTask(String folderPath,int priority){
+        this.folderPath = folderPath;
+        this.priority = priority;
+    }
+
+    public String getFolderPath(){
+        return folderPath;
+    }
+
+    public int getPriority(){
+        return priority;
+    }
+
+    @Override
+    public int compareTo(BackupTask other){
+        return other.priority - this.priority;
+    }
+}
